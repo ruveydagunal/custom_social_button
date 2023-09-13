40dc0d387b8370631c2f8a082d881b6e9e0d9a37
ruveydagunal <136679910+ruveydagunal@users.noreply.github.com>
2023-09-13 11:14:02 +0300

    update: color change

--- lib/custom_social_button.dart
+++ lib/custom_social_button.dart
@@ -1,13 +1,16 @@
 import 'package:flutter/material.dart';
 
 class CustomSocialButton extends StatelessWidget {
-  const CustomSocialButton({super.key});
+  final Color bgColor;
+  const CustomSocialButton({super.key, required this.bgColor});
+  
   @override
   Widget build(BuildContext context) {
+    
     return Scaffold(
       body: Center(
         child: CircleAvatar(
-            backgroundColor: Colors.blue,
+            backgroundColor: bgColor,
             child: IconButton(icon: Icon(Icons.person), onPressed: () {},),
           ), 
       ) ,
